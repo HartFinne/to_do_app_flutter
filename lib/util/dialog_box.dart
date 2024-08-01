48f90ee5e45ea388e41307a2c6805ab643020776
HartFinne <167497706+HartFinne@users.noreply.github.com>
2024-08-01 22:39:39 +0800

    done with to do app but do not understand some concept

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:to_do_app/util/my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+  DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow,
+      content: Container(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // to get user input
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new task",
+              ),
+            ),
+
+            // buttons -> save + cancel
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // save
+                MyButton(text: "Save", onPressed: onSave),
+
+                // for gap
+                const SizedBox(
+                  width: 10,
+                ),
+                // cancel
+                MyButton(text: "Cancel", onPressed: onCancel),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
